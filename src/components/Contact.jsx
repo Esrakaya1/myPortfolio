ac3d69b57552171f412abbf1256ac7fd93c9b898
Esrakaya1 <kyesra7@gmail.com>
2025-05-23 11:25:53 +0300

    changed contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,20 @@
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import { useState } from "react";
 
 const SERVICE_ID = "service_pec49ep";
 const TEMPLATE_ID = "template_yqjdoth";
 const PUBLIC_KEY = "IVSgZIZealhvHTiQo";
 
 const Contact = () => {
+  const [success, setSuccess] = useState(false);
   const handleOnSubmit = (e) => {
     e.preventDefault();
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
       (result) => {
         console.log(result.text);
-        alert("Message Sent Successfully");
+        setSuccess(true);
+        setTimeout(() => setSuccess(false), 3000);
       },
       (error) => {
         console.log(error.text);
@@ -21,59 +24,6 @@ const Contact = () => {
     e.target.reset();
   };
   return (
-    // <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-gray-900 via-gray-800 to-black px-4">
-    //   <motion.h2 className="my-10 text-center text-4xl">Get in Touch</motion.h2>
-    //   <p className="text-gray-300 text-center">
-    //     Send me a message to get in touch.
-    //   </p>
-
-    //   <motion.form
-    //     className="bg-white/10 backdrop-blur-lg rounded-xl p-8 shadow-xl max-w-xl mx-auto"
-    //     onSubmit={handleOnSubmit}
-    //   >
-    //     <div className="formElement flex flex-col">
-    //       <label className="mb-1">Name</label>
-    //       <input
-    //         type="text"
-    //         id="from_name"
-    //         name="from_name"
-    //         placeholder=" Your name.."
-    //         required
-    //         className="rounded h-10"
-    //       />
-    //     </div>
-
-    //     <div className="formElement flex flex-col">
-    //       <label className="mb-1">E-mail</label>
-    //       <input
-    //         type="email"
-    //         id="from_email"
-    //         name="from_email"
-    //         placeholder=" Your email.."
-    //         required
-    //         className="rounded h-10"
-    //       />
-    //     </div>
-
-    //     <div className="formElement flex flex-col">
-    //       <label className="mb-1">Message</label>
-    //       <textarea
-    //         name="message"
-    //         rows="8"
-    //         cols="30"
-    //         placeholder=" Your message.."
-    //         required
-    //         className="rounded h-30"
-    //       />
-    //     </div>
-    //     <button
-    //       type="submit"
-    //       className="w-full py-2 px-4 bg-gradient-to-r from-purple-500 to-indigo-600 text-white rounded-md hover:from-purple-600 hover:to-indigo-700 flex items-center justify-center gap-2 transition-all"
-    //     >
-    //       Submit
-    //     </button>
-    //   </motion.form>
-    // </div>
     <div className="min-h-screen flex flex-col items-center justify-center px-4 relative z-10 text-white">
       <div className="mb-8 text-center">
         <motion.h2
@@ -125,6 +75,29 @@ const Contact = () => {
           <span className="inline-block transform rotate-45">➤</span>
         </button>
       </motion.form>
+
+      {success && (
+        <div className="fixed top-6 right-6 z-50 flex items-center justify-center gap-3 rounded-md bg-purple-600 px-4 py-2 text-white shadow-lg transition-all duration-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5 13l4 4L19 7"
+            />
+          </svg>
+          <span className="text-sm font-medium">
+            Thanks for your interest! I’ll respond to your message as soon as
+            possible.
+          </span>
+        </div>
+      )}
     </div>
   );
 };
